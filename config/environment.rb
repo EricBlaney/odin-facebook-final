3ddf031dcee7d16c735a6f36dec47d21675974a1
EricBlaney <EricBlaney@gmail.com>
2021-08-18 12:20:44 -0400

    Deleted stray text

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-#api key for send grid
-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
